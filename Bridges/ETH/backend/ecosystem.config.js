b3b6b840ecdf6d4144fa7a94b88f9c8739fe5dc4
hawkofsky <hawkofsky112@outlook.com>
2021-12-01 22:57:15 +0800

    change pm2 config and update mockdelo token contract

--- Bridges/ETH/backend/ecosystem.config.js
+++ Bridges/ETH/backend/ecosystem.config.js
@@ -3,11 +3,11 @@ module.exports = {
     name   : "subscribe:bsc",
     script : "node ace subscribe:event:bsc",
     watch  : false,
-    cwd    : "/home/site/repository/decentra-lotto/Bridges/ETH/backend/"
+    cwd    : "/home/azureuser/decentra-lotto/Bridges/ETH/backend"
   }, {
     name   : "subscribe:eth",
     script : "node ace subscribe:event:eth",
     watch  : false,
-    cwd    : "/home/site/repository/decentra-lotto/Bridges/ETH/backend/"
+    cwd    : "/home/azureuser/decentra-lotto/Bridges/ETH/backend"
   }]
 }
